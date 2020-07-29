edcef0839e78a90e22c7e218c9e0f77860b59325
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-29 19:40:40 -0400

    Fixed Issue 08

--- scripts/api/hotelApi.js
+++ scripts/api/hotelApi.js
@@ -1,4 +1,18 @@
-function getHotelInfo(settings) {
+function getHotelInfo(city) {
+  // Hotel api info
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url:
+      "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" +
+      city,
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "hotels4.p.rapidapi.com",
+      "x-rapidapi-key": "5b3926684fmshb888e3231348c52p1496d1jsn3859b4010f0d",
+    },
+  };
+
   $.ajax(settings).done(function (response) {
     console.log(response);
   });
--- scripts/api/weatherApi.js
+++ scripts/api/weatherApi.js
@@ -1,8 +1,13 @@
 // This is for the weather api only aspects of the website
 
-function getMainForecast(queryUrl) {
+function getMainForecast(city, isHotel, isRestaurant) {
+  let queryUrlMain =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=b69a42c83210378fa102751081b2696f";
+
   $.ajax({
-    url: queryUrl,
+    url: queryUrlMain,
     method: "GET",
   }).then(function (response) {
     console.log(response);
@@ -12,3 +17,8 @@ function getMainForecast(queryUrl) {
 // grab 5 day forecast
 
 // display weather info here
+
+// check if the isHotel and/or isRestaurant are true or false
+
+// getHotelInfo(city);
+// getRestaurantInfo(lat, lon);
