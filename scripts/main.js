b56a6777691d6c4d8c481561172dc1dfe8cfae62
Cristian Hornedo <officialcristianl@gmail.com>
2020-08-01 11:53:48 -0400

    fixed issue with city name not being grabbed

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
   console.log("now", now);
 
   // hold the bool value of the check boxes (set to true in the beginning but can be switched to false if user unchecks the box)
-  let isHotelAccepted = false;
-  let isRestaurantAccepted = false;
 
   // Prevent the enter button from leading to a page not wanted
   $(document).on("submit", (event) => {
@@ -16,16 +14,21 @@ $(document).ready(function () {
 
   // When user presses enter they will start the chain of events
   $(document).on("submit", ".search-bar", () => {
-    let cityName = $("input").val();
+    let cityName = $("#search").val();
+
     console.log(cityName);
     getMainForecast(cityName, isHotelAccepted, isRestaurantAccepted);
   });
 
   $(document).on("click", ".search-icon", () => {
-    let cityName = $("input").val();
+    let cityName = $("#search").val();
 
     getMainForecast(cityName, isHotelAccepted, isRestaurantAccepted);
   });
 
   $("select").formSelect();
+  let isHotelAccepted = $("select").formSelect($("#hotelOption")[0][0]);
+  console.log("isHotelAccepted", isHotelAccepted);
+  let isRestaurantAccepted = $("select").formSelect($("#restaurantOption"));
+  console.log("isRestaurantAccepted", isRestaurantAccepted);
 });
