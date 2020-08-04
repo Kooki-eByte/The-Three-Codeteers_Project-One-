2d502d32c93d419737bbf657267ab41a98b09ae5
Cristian Hornedo <officialcristianl@gmail.com>
2020-08-04 01:02:21 -0400

    fixed side issues

---
+++ scripts/localStorage.js
@@ -0,0 +1,17 @@
+//  <div class="row weather-card">
+{/* <h5 class="fiveDayText">5 Day Forecast</h5>
+          
+<div id="fiveDay-0" class="5-day-forecast-0 col">
+  <div class="cardZero"> */}
+// 
+// <div id="fiveDay-1" class="5-day-forecast-1 col">
+{/* <div class="cardOne"> */}
+
+{/* <div id="fiveDay-2" class="5-day-forecast-2 col">
+              <div class="cardTwo"></div> */}
+
+{/* <div id="fiveDay-3" class="5-day-forecast-3 col">
+              <div class="cardThree"></div> */}
+
+{/* <div id="fiveDay-4"class="5-day-forecast-4 col">
+              <div class="cardFour"></div> */}
