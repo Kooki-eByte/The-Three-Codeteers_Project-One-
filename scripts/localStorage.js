7abc35eae762f364992c1fd265e66ba3e2e76559
Cristian Hornedo <officialcristianl@gmail.com>
2020-08-04 14:02:26 -0400

    Fixed Issue #27

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -2,11 +2,12 @@
 let userInfo = JSON.parse(localStorage.getItem("userInfo"));
 let submitBtn = $("#submit-btn");
 let usersInfoArr = [];
-$(document).on("click", function (event) {
+
+$(document).on("submit", function (event) {
   event.preventDefault();
 });
 
-function getUsersInfo() {
+function getUsersInfo(firstName, lastName, email, message) {
   let userInfoObj = {
     firstname: firstName,
     lastname: lastName,
@@ -26,10 +27,10 @@ function clearBoard() {
 }
 
 $(document).on("click", "#submit-btn", () => {
-  let firstName = $("#first_name").val();
-  let lastName = $("#last_name").val();
-  let email = $("#email").val();
-  let message = $("#message").val();
+  let firstName = $("#first_name").val().trim();
+  let lastName = $("#last_name").val().trim();
+  let email = $("#email").val().trim();
+  let message = $("#message").val().trim();
   if (firstName === "" || lastName === "" || email === "" || message === "") {
     return false;
   } else {
